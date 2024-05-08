ac02ba7eab061dfdb2321f1c8150a0e797304ea6
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-05-08 14:45:56 +0900

    モデルのテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,25 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors.full_messages).to include("Last_name can't be blank")
     end
-
-
+    it 'first_nameが空では登録できない' do
+      user = User.new(nickname: 'test', email: 'test@example', password: '000000', password_confirmation: '000000',last_name: 'test', first_name: '',last_name_kana: 'test',first_name_kana: 'test',birthday: '00/00/00',)
+      user.valid?
+      expect(user.errors.full_messages).to include("First_name can't be blank")
+    end
+    it 'last_name_kanaが空では登録できない' do
+      user = User.new(nickname: 'test', email: 'test@example', password: '000000', password_confirmation: '000000',last_name: 'test', first_name: 'test',last_name_kana: '',first_name_kana: 'test',birthday: '00/00/00',)
+      user.valid?
+      expect(user.errors.full_messages).to include("Last_name_kana can't be blank")
+    end
+    it 'first_name_kanaが空では登録できない' do
+      user = User.new(nickname: 'test', email: 'test@example', password: '000000', password_confirmation: '000000',last_name: 'test', first_name: 'test',last_name_kana: 'test',first_name_kana: '',birthday: '00/00/00',)
+      user.valid?
+      expect(user.errors.full_messages).to include("First_name_kana can't be blank")
+    end
+    it 'birthdayが空では登録できない' do
+      user = User.new(nickname: 'test', email: 'test@example', password: '000000', password_confirmation: '000000',last_name: 'test', first_name: 'test',last_name_kana: 'test',first_name_kana: 'test',birthday: '',)
+      user.valid?
+      expect(user.errors.full_messages).to include("Birthday can't be blank")
+    end
+  end
 end
